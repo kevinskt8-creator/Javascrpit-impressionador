759c08460af6e11bd65e2a0575fe97be5bde6dfb
kevinskt8-creator <kevinskt8@gmail.com>
2026-01-31 17:50:00 -0300

    dados

---
+++ aula6.js
@@ -0,0 +1,23 @@
+//STRINGS//
+
+const texto1 ='Aqui temos uma string normal';
+const texto2 = "Já essa, apesar das aspas duplas invés das simples, também é uma string normal";
+
+const numeroQuecQueroRegistrar = 31472;
+const stringTemplate = `O número que decidi registrar foi o ${numeroQuecQueroRegistrar}`;
+
+//Console.log(StringTemplate) fará aparecer na tela o seguinte texto:
+// O número que decidir resgistrar foi 0 31472
+
+console.log(stringTemplate);
+
+//NUMBER//
+
+const numero1 = 27;
+const numero2 = 23.25;
+const numero3 = -40;
+const total = numero1 + numero2 + numero3;
+
+console.log(total);
+
+//10.25
