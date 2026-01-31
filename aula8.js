5ba4b487d5f0b09374a8706389204e06b1937acb
kevinskt8-creator <kevinskt8@gmail.com>
2026-01-31 19:41:04 -0300

    function

---
+++ aula8.js
@@ -0,0 +1,8 @@
+function pegarComanda() {
+console.log("Olá, boa noite!");
+console.log("Pegue aqui a sua comanda!");
+console.log("Bom apetite!!");
+console.log('-------------------------')
+}
+
+pegarComanda();
