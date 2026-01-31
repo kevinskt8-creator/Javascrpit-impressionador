916f5db4c5e6877efd7d3efe7853ec1f920dc88f
kevinskt8-creator <kevinskt8@gmail.com>
2026-01-31 20:44:49 -0300

    ok

---
+++ aula9.js
@@ -0,0 +1,12 @@
+function calcularQuadrado(numeroDaVez) {
+    console.log(`O numero da vez é o ${numeroDaVez}`);
+    console.log(`O número da vez (${numeroDaVez}) ao quadrado vale ${numeroDaVez ** 2}`);
+}
+
+calcularQuadrado(1);
+calcularQuadrado(2);
+calcularQuadrado(3);
+calcularQuadrado(4);
+calcularQuadrado(5);
+calcularQuadrado(6);
+calcularQuadrado(7);
