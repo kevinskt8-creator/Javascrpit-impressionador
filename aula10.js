0b7217653a18243a26864bd8650aba83923b36a3
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-01 13:42:16 -0300

    aula 10 function

---
+++ aula10.js
@@ -0,0 +1,10 @@
+function resolverBhaskara() {}
+
+function calcularRaizQuadrada(base) {
+    
+console.log('Eu queria escrever isso aqui na tela. Será que eu consigo?');
+return base**(1/2);
+ 
+}
+
+let valorRaizQuadrada = calcularRaizQuadrada(4);
