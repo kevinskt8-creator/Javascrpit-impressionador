8a2f4b0aaa9b6b1b3f7877471192b6b450c4a57e
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-02 18:27:08 -0300

    Operador Ternário

---
+++ aula18.js
@@ -0,0 +1,22 @@
+const usuario = {nome: 'Carlos', idade: 17, time: 'Flamengo'};
+
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
+const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro!';
+const mensagemDeBoasVindas3 = 'Bem vindo, torcedor Vascaíno!';
+const mensagemDeBoasVindasGeral = 'Bem vindo, amante do esporte!';
+
+// Não se costuma usar operador ternário quando se tem mais de duas condições//
+
+usuario.time === 'Fluminense' && usuario.idade >= 18
+? console.log(mensagemDeBoasVindas1)
+: usuario.time === 'Fluminense' && usuario.idade >= 18
+? console.log(mensagemDeBoasVindas2)
+: usuario.idade >= 18
+? console.log(mensagemDeBoasVindasGeral)
+: console.log('Não vendemos ingresso para torcedores menores de 18 anos');
+
+const mensagemDeEscolha = 'Clique no setor para o qual deseja comprar seu ingresso!';
+const mensagemFinal = 'Divirta-se no Maracanã!!!';
+
+console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
