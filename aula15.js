f2b43a5829532e8cfde78ea8d9cb425f6895524b
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-02 16:16:36 -0300

    ok

--- aula15.js
+++ aula15.js
@@ -1,10 +1,16 @@
+const usuario = {nome: 'Carlos', idade: 29, time: 'Flamengo'};
+
 const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
 const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro';
 
+if (usuario.time === 'Fluminense') {
 console.log(mensagemDeBoasVindas1);
+}
+if (usuario.time === 'Flamengo') {
 console.log(mensagemDeBoasVindas2);
+}
 
-const mensagemEscolha = 'Clique no setor para o qual deseja comprar seu ingresso!';
+const mensagemDeEscolha = 'Clique no setor para o qual deseja comprar seu ingresso!';
 const mensagemFinal = 'Divirta-se no Maracanã!!!';
 
 console.log(mensagemDeEscolha);
