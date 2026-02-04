818e71fb5c19ab2c04c8e033864de0a3e2c1d747
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-04 16:35:40 -0300

    While sequencia fibonacci

---
+++ aula20.js
@@ -0,0 +1,21 @@
+function calculateFibonacciTerm(termNumber) {
+    let lastTerm = 1;
+    let secondToLast = 0;
+
+    if (termNumber === 0) {
+        return 0;
+    }
+    if (termNumber === 1) {
+        return 1;
+    }
+    let currentIteration = 2;
+    while (currentIteration <= termNumber) {
+        let auxvar = lastTerm + secondToLast;
+        secondToLast = lastTerm;
+        lastTerm = auxvar;
+        currentIteration++;
+    }
+    return lastTerm;
+}
+
+console.log(`O valor do termo número 6 da sequencia de fibonacci é ${calculateFibonacciTerm(6)}`);
